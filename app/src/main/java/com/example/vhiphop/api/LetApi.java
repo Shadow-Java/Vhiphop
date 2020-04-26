3fe22330b7c61847c7ffc75815006f0fe4ba9451
liyuanbo1997 <1723928492@qq.com>
2020-04-26 14:59:18 +0800

    detaillistfragment add recyclerview,and add siteapi

---
+++ app/src/main/java/com/example/vhiphop/api/LetApi.java
@@ -0,0 +1,7 @@
+package com.example.vhiphop.api;
+
+/*
+ *作者：created by 影子 on 2020/4/26 14:39
+ *邮箱：1723928492@qq.com
+ */public class LetApi extends BaseSiteApi {
+}
---
+++ app/src/main/java/com/example/vhiphop/api/SohuApi.java
@@ -0,0 +1,7 @@
+package com.example.vhiphop.api;
+
+/*
+ *作者：created by 影子 on 2020/4/26 14:40
+ *邮箱：1723928492@qq.com
+ */public class SohuApi extends BaseSiteApi {
+}
